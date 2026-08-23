29ff967addc96093747651657996ef70f4974cc4
Vijay Ravula <144901255+VijayRavula27@users.noreply.github.com>
2026-08-23 14:45:30 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int top = 0;
+        int bottom = matrix.size()-1;
+        int row = -1;
+
+        while(top <= bottom){
+            int mid = top + (bottom - top) / 2;
+
+            if(matrix[mid][0] <= target && matrix[mid][matrix[mid].size()-1] >= target){
+                row = mid;
+                break;
+            } else if(matrix[mid][0] > target){
+                bottom = mid - 1;
+            } else {
+                top = mid + 1;
+            }
+        }
+        if(row == -1) return false;
+
+        int left = 0;
+        int right = matrix[row].size()-1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(matrix[row][mid] == target){
+                return true;
+            } else if(matrix[row][mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
