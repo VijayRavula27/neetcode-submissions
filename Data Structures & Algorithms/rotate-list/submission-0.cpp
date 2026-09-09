26ad489806c11597522b7172a44dbe64ef75c8ca
Vijay Ravula <144901255+VijayRavula27@users.noreply.github.com>
2026-09-09 22:47:04 +0530

    Add: rotate-list - submission-0

---
+++ Data Structures & Algorithms/rotate-list/submission-0.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == nullptr) return head;
+
+        ListNode* temp = head;
+        int length = 1;
+
+        while(temp->next != nullptr){
+            temp = temp->next;
+            length++;
+        }
+        int position = k % length;
+        if(position == 0) return head;
+
+        ListNode* curr = head;
+        for(int i = 0; i < length - position - 1; i++){
+            curr = curr->next;
+        }
+        ListNode* newhead = curr->next;
+        curr->next = nullptr;
+
+        temp->next = head;
+
+        return newhead;
+    }
+};
